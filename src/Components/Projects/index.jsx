7c626b4d0072e6a1b41b5d619a1264279681e956
jancoweb <jancoweb@outlook.com>
2022-12-03 02:27:24 -0300

    new project

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -42,8 +42,27 @@ function Projects() {
             <img src="https://i.imgur.com/2uUMV7e.png" alt="" />
           </div>
         </div>
+        <div className="project right">
+          <div className="project-text-content">
+            <h2 className="project-title">Rick and Morty App</h2>
+            <p className="project-text">
+              I started this project to pratice ReactJS and third party API consumption. In this case, this application allows you to find a specific Rick and Morty character by his name, search a random character or even see the full cast (826 characters).
+            </p>
+            <span>React, Javascript</span>
+            <br />
+            <br />
+            <a href="https://github.com/j-efe/front-integral-m05-desafio-t06" target='blank'>Github front-end Repository</a>
+            <br />
+            <br />
+            <a href="https://rickandmorty-jancoweb.netlify.app/" target='blank'>Application live on Netlify!</a>
+          </div>
+          <div className='project-img'>
+            <img src="https://i.imgur.com/piZdqII.png" alt="" />
+          </div>
+        </div>
         <p className='git'>You can find more projects on my <a href="https://github.com/jancoweb" target='blank'>GitHub</a></p>
       </div>
+
     </div>
   )
 }
